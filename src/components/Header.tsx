1e8c4485f6acb0297f0b62249fcc5b7a31ec9cb7
zehan12 <zehan9211@gmail.com>
2024-07-25 00:18:39 +0530

    add navigation

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Header = () => {
+    const pathName = usePathname();
+
+    return (
+        <header className="w-full h-10 flex items-center">
+            <nav className="w-8/12 mx-auto flex justify-between">
+                <div className="text-2xl font-mono text-orange-700">
+                    zehan khan
+                </div>
+                <ul className="flex gap-5 text-2xl font-mono text-purple-500">
+                    <li>
+                        <Link
+                            className={cn(
+                                pathName === "/" ? "text-green-500" : ""
+                            )}
+                            href={"/"}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={cn(
+                                pathName === "/work" ? "text-green-500" : ""
+                            )}
+                            href={"/work"}
+                        >
+                            Work
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
