2d909a70dfb8ce2c293adf86486ff9ebf27c9ded
zehan12 <zehan9211@gmail.com>
2024-11-20 20:41:12 +0530

    update framer-motion with motion package

--- src/components/shared/theme-toggle.tsx
+++ src/components/shared/theme-toggle.tsx
@@ -1,6 +1,6 @@
 
 "use client";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 import { CloudMoon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
