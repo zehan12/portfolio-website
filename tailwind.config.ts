1ef438c0dada8db3ce257bcfcd0ea0c68a13233a
zehan12 <zehan9211@gmail.com>
2024-12-06 22:55:11 +0530

    add custom css and animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,78 @@ export default {
           '"Noto Color Emoji"',
         ],
       },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0%",
+          },
+          "75%": {
+            opacity: "0%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        "fade-left": {
+          "0%": {
+            transform: "translateX(100%)",
+            opacity: "0%",
+          },
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        "fade-right": {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: "0%",
+          },
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        title: {
+          "0%": {
+            "line-height": "0%",
+            "letter-spacing": "0.25em",
+            opacity: "0",
+          },
+          "25%": {
+            "line-height": "0%",
+            opacity: "0%",
+          },
+          "80%": {
+            opacity: "100%",
+          },
+          "100%": {
+            "line-height": "100%",
+            opacity: "100%",
+          },
+        },
+        color: {
+          "0%": { color: "#5468ff" },
+          "20%": { color: "#CB9DF0" },
+          "40%": { color: "#F0C1E1" },
+          "60%": { color: "#FDDBBB" },
+          "80%": { color: "#FFF9BF" },
+          "100%": { color: "#A8CD89" },
+        },
+      },
+      animation: {
+        title: "title 3s ease-out forwards",
+        "fade-in": "fade-in 3s ease-in-out forwards",
+        "fade-left": "fade-left 3s ease-in-out forwards",
+        "fade-right": "fade-right 3s ease-in-out forwards",
+        color: "color 5s infinite alternate",
+      },
     },
   },
   plugins: [],
